d8c58b4ce32fef0a09da95c50499840bddbb3eaf
Prachi Yadav <prachiiyadav2409@gmail.com>
2025-05-03 20:20:21 +0530

    added message controller nd routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth-route.js";
+import messageRoutes from "./routes/message-route.js";
+// import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-import authRoutes from "./routes/auth-route.js";
-import { connectDB } from "./lib/db.js";
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-const app = express();
-
-// Middleware to parse JSON requests
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
-// Auth routes
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-// Define the port
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// Start the server and connect to the database
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
