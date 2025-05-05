2aa033cc9e88b0ff253b5efb6d8bce3d184eb0e0
raunakdeveloper <raunakkaushal.rk@gmail.com>
2025-05-05 19:18:10 +0530

    fixxs

--- backend/src/routes/message-route.js
+++ backend/src/routes/message-route.js
@@ -8,9 +8,11 @@ import {
 
 const router = express.Router();
 
-router.get("/users", protectRoute, getUsersForSidebar);
-router.get("/:id", protectRoute, getMessages);
 
-router.post("/send/:id", protectRoute, sendMessage);
+router.get("/message/:id", protectRoute, getMessages);
+router.post("/send/message/:id", protectRoute, sendMessage);
+
+
+router.get("/users", protectRoute, getUsersForSidebar);
 
 export default router;
