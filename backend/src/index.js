fef7e80b49115e690049a3d3a01a0600cc32b7d2
raunakdeveloper <raunakkaushal.rk@gmail.com>
2025-05-05 19:12:08 +0530

    add files

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,22 +2,28 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
+import http from "http"; // to create HTTP server
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth-route.js";
 import messageRoutes from "./routes/message-route.js";
-import { app, server } from "./lib/socket.js";
+import { io } from "./lib/socket.js"; // Import io
 
 dotenv.config();
+
+const app = express();
+const server = http.createServer(app); // Wrap express app in HTTP server
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
+// Attach io to server
+io.attach(server);
+
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -25,18 +31,19 @@ app.use(
   })
 );
 
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+//Production: Serve frontend
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
+  console.log("Server is running on PORT:" + PORT);
   connectDB();
 });
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,22 +1,16 @@
 import { Server } from "socket.io";
-import http from "http";
-import express from "express";
 
-const app = express();
-const server = http.createServer(app);
-
-const io = new Server(server, {
+const io = new Server({
   cors: {
     origin: ["http://localhost:5173"],
   },
 });
 
+const userSocketMap = {};
+
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
-
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
@@ -24,7 +18,6 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
@@ -34,4 +27,4 @@ io.on("connection", (socket) => {
   });
 });
 
-export { io, app, server };
+export { io };
